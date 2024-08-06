5335f7081be1099736be171750bb5101112ee877
Vainaupang <vaithangteaa@gmail.com>
2024-08-06 12:51:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -25,7 +25,7 @@ global.caption = process.env.CAPTION || global.caption || "ᴘᴏᴡᴇʀᴇᴅ 
 
 
 global.devs = "916909438834" // Developer Contact
-global.sudo = process.env.SUDO ? process.env.SUDO.replace(/[\s+]/g, '') : "null";
+global.sudo = process.env.SUDO ? process.env.SUDO.replace(/[\s+]/g, '') : "VAIPA";
 global.owner= process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.replace(/[\s+]/g, '') : "916909438834";
 
 
